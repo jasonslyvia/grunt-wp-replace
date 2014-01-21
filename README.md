653862bda4589e95ebc6c351d76ec388e2eec180
jasonslyvia <jasonslyvia@gmail.com>
2014-01-22 03:28:19 +0800

    分离替换过程，重用代码

--- README.md
+++ README.md
@@ -26,61 +26,51 @@ In your project's Gruntfile, add a section named `wp_replace` to the data object
 grunt.initConfig({
   wp_replace: {
     options: {
-      // Task-specific options go here.
+      templatePath: '/wp-content/themes/your-theme/',
+      jsPath: '/wp-content/themes/your-theme/js-dist/',
+      cssPath: '/wp-content/themes/your-theme/css-dist/',
+      concat: [{
+        src: ['a.js', 'b.js'],
+        dest: ['concated.js']
+      }]
     },
     your_target: {
-      // Target-specific file lists and/or options go here.
-    },
+      src: ['header-src.php', 'footer-src.php']
+    }
   },
 });
 ```
 
 ### Options
 
-#### options.separator
+#### options.templatePath
 Type: `String`
-Default value: `',  '`
+Default value: `''`
 
-A string value that is used to do something with whatever.
+the template path of your wordpress theme
 
-#### options.punctuation
+#### options.jsPath
 Type: `String`
+Default value: `''`
+
+the path which javascript files located in your wordpress theme
+
+#### options.cssPath
+Type: `String`
+Default value: `''`
+
+the path which css files located in your wordpress theme
+
+#### options.concat
+Type: `Array`
 Default value: `'.'`
 
-A string value that is used to do something else with whatever else.
+if you concat any files, define them in order to replace their replace, see the `Overview` part for format
 
 ### Usage Examples
 
 #### Default Options
-In this example, the default options are used to do something with whatever. So if the `testing` file has the content `Testing` and the `123` file had the content `1 2 3`, the generated result would be `Testing, 1 2 3.`
-
-```js
-grunt.initConfig({
-  wp_replace: {
-    options: {},
-    files: {
-      'dest/default_options': ['src/testing', 'src/123'],
-    },
-  },
-});
-```
-
-#### Custom Options
-In this example, custom options are used to do something else with whatever else. So if the `testing` file has the content `Testing` and the `123` file had the content `1 2 3`, the generated result in this case would be `Testing: 1 2 3 !!!`
-
-```js
-grunt.initConfig({
-  wp_replace: {
-    options: {
-      separator: ': ',
-      punctuation: ' !!!',
-    },
-    files: {
-      'dest/default_options': ['src/testing', 'src/123'],
-    },
-  },
-});
-```
+TODO
 
 ## Contributing
 In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
--- tasks/wp_replace.js
+++ tasks/wp_replace.js
@@ -10,41 +10,134 @@
 
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+  grunt.registerMultiTask('wp-replace', 'replace filereved or concated assets filename', function(){
+    var options = this.options();
+    var jsPath = options.jsPath;
+    var cssPath = options.cssPath;
+    var templatePath = options.templatePath;
 
-  grunt.registerMultiTask('wp_replace', 'Replace revved or concated filename for Wordpress source file.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+    //简化filerev保存的文件名修改信息，去掉多余的路径信息，只保留新旧文件名
+    //形如 {oldFileName.css : newFileName.css}
+    var newFiles = {};
+    for(var i in grunt.filerev.summary){
+      var newKey = i.split('/').pop();
+      newKey = newKey.replace(/\.min/i, '');
+      var newValue = grunt.filerev.summary[i].split('/').pop();
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
+      newFiles[newKey] = newValue;
+    }
+
+    //针对每一个文件进行遍历，依次进行正则匹配
+    this.files.forEach(function(file){
+      //首先确保文件存在
+      file.src.filter(function(filePath){
+        if (!grunt.file.exists(filePath)) {
+          grunt.log.warn('source file ' + filePath + ' not exist');
           return false;
-        } else {
+        }
+        else{
           return true;
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+      }).forEach(function(filePath){
 
-      // Handle options.
-      src += options.punctuation;
+        //获得文件内容
+        var content = grunt.file.read(filePath);
+        var cssRegExp = /(<link.*?href=[\'\"])(.*?)([\'\"][^>]*>)/gi;
+        var jsRegExp = /(<script.*?src=[\'\"])(.*?)([\'\"][^>]*?>\s*<\/script>)/gi;
+        var concat = options.concat;
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+        //进行静态资源引用替换
+        var updated = replaceReference(content, cssRegExp, 'css', cssPath, newFiles, concat);
+        updated = replaceReference(content, jsRegExp, 'js', jsPath, newFiles, concat);
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+        //如果替换了新的引用，则再次写入文件
+        if (updated) {
+          grunt.log.ok('assets are replaced in '+filePath);
+          filePath = filePath.replace(/-src/i, '');
+          grunt.file.write(filePath, content);
+        }
+      });
     });
   });
+
+/*
+ *  替换文件中出现的静态资源引用，返回文件是否已更新的布尔值
+ *
+ *  @param {string} content 文件内容
+ *  @param {object} regExp 进行匹配的正则对象
+ *  @param {string} type 进行匹配的资源类型，js或css
+ *  @param {string} filePath 静态资源的引用uri，形如/wp-content/themes/your-theme/css/
+ *  @param {array} revvedFiles 包含了新旧文件名的对象数组，
+ *                             参考 grunt.filerev.summary，
+ *                             这里使用简化版本
+ *  @param {array} concat 包含了concat前后文件名的对象数组
+ *  @return {bool}
+ */
+function replaceReference(content, regExp, type, filePath, revvedFiles, concat){
+
+  var updated = false,
+      assetCount = 0;
+
+  //WARN：若文件中多次出现同一资源的引入，同时存在concat后的资源，
+  //      则可能出现源文件中将多行内容被替换为1行的情况，
+  //      正则匹配时就会跳过若干个字符，参考js regexp.lastIndex
+  var match;
+  while((match = regExp.exec(content)) !== null){
+    var asset = match[2].split('/').pop();
+
+    //若该asset被引用
+    if (revvedFiles.hasOwnProperty(asset)) {
+
+      //判断其是否被concat
+      var concated = concat.every(function(el, i){
+        //若被concat
+        if(el.src.join(' ').indexOf(asset) !== -1){
+          //若已替换则直接删除本条引用
+          if (el.replaced) {
+            content = content.replace(new RegExp(match[0], "i"), '');
+            //改变lastIndex确保匹配到文件中的每一个字符
+            regExp.lastIndex -= match[0].length;
+            grunt.log.debug('remove source file ' + asset + ' that already concated');
+          }
+          //否则将concat后对应的文件替换，同时添加已替换属性
+          else{
+            //若指定的concat后的文件不存在
+            if (!grunt.file.exists(filePath+revvedFiles[el.dest])) {
+              grunt.fail.warn('concat file '+el.dest+' for '+el.src.join(',')+' does not exist!');
+            }
+
+            var concatedAsset = match[1] + filePath + revvedFiles[el.dest] + match[3];
+            content = content.replace(new RegExp(match[0], "i"), concatedAsset);
+            //改变lastIndex确保匹配到文件中的每一个字符
+            regExp.lastIndex -= (match[0].length - concatedAsset.length);
+            el.replaced = true;
+
+            grunt.log.debug('replace concat file '+revvedFiles[el.dest]+' for '+asset);
+
+            assetCount++;
+            updated = true;
+          }
+
+          //不考虑a,b被concat为x，同时a,d被concat为y的情况
+          //不再继续遍历concat
+          return false;
+        }
+        return true;
+      });
+
+      //concated为true说明concat.every全部return true，即
+      //该js并未被concat
+      //因此直接替换为filerev后的文件即可
+      if (concated) {
+        content = content.replace(new RegExp(match[0], "i"), match[1] + filePath + revvedFiles[asset] + match[3]);
+        updated = true;
+        assetCount++;
+        grunt.log.debug(type+' replace: ' + asset + ' : ' + revvedFiles[asset]);
+      }
+    }
+  }
+
+  return updated;
+}
 
 };
